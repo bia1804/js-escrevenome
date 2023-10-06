899fa715432b80a0000f9dceb7bc971ecf659605
bia1804 <143807095+bia1804@users.noreply.github.com>
2023-10-06 09:03:46 -0300

    adicionando arquivos js e css

---
+++ sktech.js
@@ -0,0 +1,15 @@
+function setup() {
+    createCanvas(600, 600);
+  background("pink");
+  }
+  
+  function draw() {
+   stroke("black");
+    fill("white");
+  
+  if(mouseIsPressed){
+  rect(mouseX, mouseY, 20, 35);
+  }
+  
+  
+  }
